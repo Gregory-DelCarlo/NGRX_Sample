c8ad6901953171ea59231f06de68c28bf8e64848
Gregory-DelCarlo <ethandelcarlo@gmail.com>
2022-09-11 14:22:46 -0700

    Setup project and add NGRX

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+// ngrx brings redux patterns to angular
+// instal using @ngrx/core @ngrx/store
+import { StoreModule } from '@ngrx/store';
+import { simpleReducer } from './simple.reducer';
+
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    // this is our root state object which handles global state
+    StoreModule.forRoot({ message: simpleReducer }),
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
