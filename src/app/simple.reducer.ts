d9212eaaf1a497a4e258b2e53048dac8bc209bc0
Gregory-DelCarlo <ethandelcarlo@gmail.com>
2022-09-11 14:23:54 -0700

    Create simple Reducer example

---
+++ src/app/simple.reducer.ts
@@ -0,0 +1,20 @@
+// use constants to define action names
+// by convention they use all caps
+// reducers are just functions that run a switch statment on the different actions
+// and return a ne state depending on the case
+import { Action } from '@ngrx/store';
+
+//define default state
+const STATE = "Hello World";
+
+// give the reducer a default state and define the two types of data sent
+export function simpleReducer(state: string = STATE, action: Action) {
+  switch(action.type) {
+    case 'SPANISH':
+      return state = 'Hola Mundo'
+    case 'FRENCH':
+      return state = 'Bonjour le monde'
+    default:
+      return state;
+  }
+}
