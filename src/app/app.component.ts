3b3a4fd1e03d16713c59806c97eb40956389db7a
Gregory-DelCarlo <ethandelcarlo@gmail.com>
2022-09-11 14:35:05 -0700

    Create basic Component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,22 @@
 import { Component } from '@angular/core';
 
+import { Store } from '@ngrx/store';
+import { Observable } from 'rxjs';
+
+/*
+we need a way to represent and change state using the UI
+
+1. We need to create a TS interface to represent our Store (State)
+1.1 The interface must be the same as the object we pass to NGModule
+2. create an Observable Object for the pieces of state you wish to add into the Component
+2.1 by convention these look like foo$
+2.2 set this to the proper piece of state using this.store.select('foo')
+3. dispatch any actions you wish to the reducer using this.store.dispatch('ACTION')
+*/
+interface AppState {
+  message: string;
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +24,20 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'NGRX_Sample';
+
+  message$: Observable<string>
+
+  constructor(private store: Store<AppState>) {
+    this.message$ = this.store.select('message')
+  }
+
+  // action creator for spanish
+  spanishMessage() {
+    this.store.dispatch({type: 'SPANISH'})
+  }
+
+  //action creator for french
+  frenchMessage() {
+    this.store.dispatch({type: 'FRENCH'})
+  }
 }
